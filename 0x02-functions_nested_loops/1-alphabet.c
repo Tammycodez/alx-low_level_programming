e153e69a1ff6a4ae8c12f23540d4b9954f1bcaf9
Tamarapreye Papanda <preye31@gmail.com>
2022-12-14 18:02:46 -0800

    Print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * prints_alphabet - prints the alphabet, in lowercase, followed by a new line
+ * print_alphabet - print the alphabet
+ * in lowercase, followed by a new line
  */
 void print_alphabet(void)
 {

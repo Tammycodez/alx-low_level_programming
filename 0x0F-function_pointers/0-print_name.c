cfcb12f5e4a2df506707e20f6806c2117a9c6a02
Tamarapreye Papanda <preye31@gmail.com>
2023-01-18 19:03:58 -0800

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,27 @@
 #include <stdlib.h>
+
 #include "function_pointers.h"
 
+
+
 /**
- * print_name - function to print name
- * @f: function pointer to the printing function
+
+ * print_name - prints a name
+
  * @name: name to print
+
+ * @f: pointer to the printing function
+
  */
 
 void print_name(char *name, void (*f)(char *))
+
 {
+
 	if (!name || !f)
+
 		return;
+
 	f(name);
+
 }
